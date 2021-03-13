e6035d4f2583eef2bb1f89633dd4cbb7c5d43cb5
Manuel Vila <mvila@3base.com>
2021-03-14 08:06:15 +0900

    cli: Implement 'install' command

---
+++ packages/cli/src/commands/install.ts
@@ -0,0 +1,48 @@
+import {existsSync} from 'fs';
+import {join, resolve} from 'path';
+
+import type {Command} from './index.js';
+import {runNPM} from './npm.js';
+import {throwError} from '../util.js';
+
+export default {
+  name: 'install',
+  options: {
+    help: {
+      aliases: ['h']
+    }
+  },
+  async handler([], {help}: {help?: boolean}, {directory, config}) {
+    if (help) {
+      console.log('Install help...'); // TODO
+      return;
+    }
+
+    if (config === undefined) {
+      throwError(`Couldn't find a Boostr configuration file`);
+    }
+
+    await install({directory, config});
+  }
+} as Command;
+
+export async function install({directory, config}: {directory: string; config: any}) {
+  await runNPMInstall(directory);
+
+  if (config.type === 'application') {
+    for (const componentDirectoryRelative of Object.values<string>(config.components)) {
+      const componentDirectory = resolve(directory, componentDirectoryRelative);
+      await runNPMInstall(componentDirectory);
+    }
+  }
+}
+
+async function runNPMInstall(directory: string) {
+  const packageFile = join(directory, 'package.json');
+
+  if (!existsSync(packageFile)) {
+    return;
+  }
+
+  await runNPM({directory, arguments: ['install']});
+}
