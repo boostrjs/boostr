ca59ea3a9214b56ab9e0c3a56ad17bf99e2fef62
Manuel Vila <mvila@3base.com>
2021-03-21 17:12:18 +0900

    web-application-ts: Add icon

--- packages/web-application-ts/template/frontend/boostr.config.mjs
+++ packages/web-application-ts/template/frontend/boostr.config.mjs
@@ -7,6 +7,8 @@ export default ({services}) => ({
     BACKEND_URL: services.backend.url
   },
 
+  iconURL: '/boostr-favicon-3NjLR7w1Mu8UAIqq05vVG3.immutable.png',
+
   stages: {
     development: {
       url: 'http://localhost:{{frontendPort}}/',
