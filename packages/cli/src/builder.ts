9bb9c7709b5fcc935082ad2d5b6714be008ca451
Manuel Vila <mvila@3base.com>
2021-06-20 13:33:57 +0900

    Update dependencies

--- packages/cli/src/builder.ts
+++ packages/cli/src/builder.ts
@@ -73,7 +73,7 @@ export async function build({
 
   const {build}: {build: typeof buildFunction} = await requireGlobalNPMPackage(
     'esbuild',
-    '0.12.8',
+    '0.12.9',
     {
       serviceName
     }
